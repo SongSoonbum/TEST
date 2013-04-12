9cd0bec2cefd05fafb946c3474403b543458d272
Whitebase <Kusuhara@Whitebase>
2013-04-13 06:28:14 +0900

    Signed-off-by: Whitebase <Kusuhara@Whitebase>

--- hello.c
+++ hello.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 void main(){
-	printf("Hello, World!!!");
-	printf("GITHUB TEST!!!");
-	printf("日本語テスト.");
-	printf("日本語テスト2.");
+    printf("Hello, World!!!");
+    printf("GITHUB TEST!!!");
+    printf("日本語テスト.");
+    printf("日本語テスト2.");
 	
-	printf("TEST1.");
-	printf("TEST2.");
+    printf("TEST1.");
+    printf("TEST2.");
+    printf("TEST3.");
 }
